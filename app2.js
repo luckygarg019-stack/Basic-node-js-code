51c76b23c47893226e7bb367ce5694df430541e8
luckygarg019-stack <luckygarg019@gmail.com>
2026-03-30 10:43:29 +0530

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+let data = "Hello, Gla is welcoming for 2026 admission";
+fs.writeFile("Sample.txt", data,{encoding: 'utf-8',flag:'w'},(err)=>{
+    if(err) throw(err);
+    console.log("File created succesfully");
+})
+fs.readFile("Sample.txt", {encoding : 'utf-8', flag:'r'}, (err, data)=>{
+    if(err) throw(err);
+    console.log(data);
+})
+fs.appendFile("Sample.txt", "\n North India's Best College", (err)=>{
+    if (err) throw(err);
+    console.log("File edited succesfully");
+})
+// const fs = require("fs");
+// fs.unlink("sample.txt", (err)=>{
+//      if(err) throw(err);
+//      console.log("file deleted sucessfully");
+// }) 
+
+// crud operation = create read update delete
+// try ke andhr true condtion or catch je andhr false cond
+
+const fs = require('node:fs');
+const constant = 'Some content!';
+try{
+    fs.writeFileSync('/Users/joe/test.txt', content);
+    // file written successfully
+} catch (err){
+    console.error(err);
+}
+
